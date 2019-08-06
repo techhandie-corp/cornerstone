a56b83e822ec15945aa9e71c62c03b4d0ee9448e
Kenneth Jones <kayceejjay@gmail.com>
2019-08-05 22:43:55 -0400

    Update build.sh + SOURCE_BRANCH & TARGET_BRANCH

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-set -e
+set -e # Exit with nonzero exit code if anything fails
+
+SOURCE_BRANCH="master"
+TARGET_BRANCH="gh-pages"
+
 DEPLOY_REPO="https://github.com/TechHandieCorp/cornerstone.git"
 # "https://${DEPLOY_SITE_TOKEN}@github.com/techandie-corp.github.io/cornerstone.git"
 function main {
@@ -8,12 +12,17 @@ get_current_site
 build_site
 }
 function clean {
+  # Remove _sit folder content & folder
 echo "cleaning _site folder"
 if [ -d "_site" ]; then rm -Rf _site; fi
 }
 function get_current_site {
 echo "getting latest site"
 git clone --depth 1 $DEPLOY_REPO _site
+cd _site
+git checkout $TARGET_BRANCH || git checkout --orphan $TARGET_BRANCH
+find -maxdepth 1 ! -name .git ! -name . | xargs rm -rf
+cd ..
 }
 function build_site {
 echo "building site"
@@ -21,14 +30,15 @@ bundle exec jekyll build
 }
 main
 
-# Actual Deployment Step
+# Actual Deployment Steps
 function deploy {
+  # Ignore pull requests
 echo "deploying changes"
 if [ -z "$TRAVIS_PULL_REQUEST" ]; then
 echo "except don't publish site for pull requests"
 exit 0
 fi
-if [ "$TRAVIS_BRANCH" != "master" ]; then
+if [ "$TRAVIS_BRANCH" != "$SOURCE_BRANCH" ]; then
 echo "except we should only publish the master branch. stopping here"
 exit 0
 fi
@@ -39,5 +49,5 @@ git add -A
 git status
 git commit -m "Lastest site built on successful travis build $TRAVIS_BUILD_NUMBER auto-pushed to
 github"
-git push $DEPLOY_REPO master:master
+git push $DEPLOY_REPO master:gh-pages
 }
